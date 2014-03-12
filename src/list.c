7f6b985b822a4740a42666876be148f1ef0934ec
Jung-Sang Ahn <jungsang.ahn@gmail.com>
2014-03-12 14:21:41 +0900

    Modify the copyright paragraph for primitive data structures
    - AVL-Tree, B+Tree, Hash table, Doubly linked list
    
    Change-Id: Id05ca22ae9705b1701e826a01dcdf57f5fe78f17

--- src/list.c
+++ src/list.c
@@ -1,18 +1,7 @@
 /* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 /*
- *     Copyright 2010 Couchbase, Inc
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Doubly Linked List
+ * (C) 2013  Jung-Sang Ahn <jungsang.ahn@gmail.com>
  */
 
 #include <stdio.h>
