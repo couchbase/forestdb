050d3d500953e49ed82564072e46fc646443b8cc
Chiyoung Seo <chiyoung.seo@gmail.com>
2014-11-24 13:23:12 -0800

    Include /usr/local for Snappy library path.
    Change-Id: Id4c85ddc055afc5a64f0f0264fba20684ec0a4c0

--- FindSnappy.cmake
+++ FindSnappy.cmake
@@ -11,27 +11,30 @@ FIND_PATH(SNAPPY_INCLUDE_DIR snappy.h
           PATHS
                ~/Library/Frameworks
                /Library/Frameworks
+               /usr/local
                /opt/local
                /opt/csw
                /opt/snappy
                /opt)
 
-FIND_LIBRARY(LIBSNAPPY
+FIND_LIBRARY(SNAPPY_LIBRARIES
              NAMES snappy
              HINTS
                  ENV SNAPPY_DIR
              PATHS
                  ~/Library/Frameworks
                  /Library/Frameworks
+                 /usr/local
                  /opt/local
                  /opt/csw
                  /opt/snappy
                  /opt)
 
-IF (LIBSNAPPY)
-  MESSAGE(STATUS "Found snappy in ${SNAPPY_INCLUDE_DIR} : ${LIBSNAPPY}")
-ELSE (LIBSNAPPY)
+IF (SNAPPY_LIBRARIES)
+  include_directories(AFTER ${SNAPPY_INCLUDE_DIR})
+  MESSAGE(STATUS "Found snappy in ${SNAPPY_INCLUDE_DIR} : ${SNAPPY_LIBRARIES}")
+ELSE (SNAPPY_LIBRARIES)
   MESSAGE(FATAL_ERROR "Can't build forestdb without Snappy")
-ENDIF (LIBSNAPPY)
+ENDIF (SNAPPY_LIBRARIES)
 
-MARK_AS_ADVANCED(SNAPPY_INCLUDE_DIR LIBSNAPPY)
+MARK_AS_ADVANCED(SNAPPY_INCLUDE_DIR SNAPPY_LIBRARIES)
