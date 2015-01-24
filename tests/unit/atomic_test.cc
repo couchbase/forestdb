c727700684e28f5abe23e2762934b2592a912a93
Chiyoung Seo <chiyoung.seo@gmail.com>
2015-01-23 18:59:58 -0800

    Add the atomic library for different platform environments.
    Change-Id: Ia1c87536e8cb2fb1b270bc64d3990ce8cbf2befb

---
+++ tests/unit/atomic_test.cc
@@ -0,0 +1,197 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2010 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "test.h"
+#include "atomic.h"
+
+#include "memleak.h"
+
+void basic_test()
+{
+    TEST_INIT();
+
+    atomic_val_t counter_64;
+    uint64_t val_64 = 0, val_64_new = 200;
+    int64_t delta_64 = 10;
+    atomic_val_init_64(&counter_64, val_64);
+    val_64 = 100;
+    atomic_val_store_64(&counter_64, val_64);
+    TEST_CHK(counter_64.value.val_64 == 100);
+    atomic_val_compare_and_set_64(&counter_64, val_64, val_64_new);
+    TEST_CHK(counter_64.value.val_64 == 200);
+    atomic_val_incr_64(&counter_64);
+    TEST_CHK(counter_64.value.val_64 == 201);
+    atomic_val_decr_64(&counter_64);
+    TEST_CHK(counter_64.value.val_64 == 200);
+    atomic_val_add_64(&counter_64, delta_64);
+    TEST_CHK(counter_64.value.val_64 = 210);
+    atomic_val_sub_64(&counter_64, delta_64);
+    TEST_CHK(counter_64.value.val_64 == 200);
+
+    atomic_val_t counter_32;
+    uint32_t val_32 = 0, val_32_new = 200;
+    int32_t delta_32 = 10;
+    atomic_val_init_32(&counter_32, val_32);
+    val_32 = 100;
+    atomic_val_store_32(&counter_32, val_32);
+    TEST_CHK(counter_32.value.val_32 == 100);
+    atomic_val_compare_and_set_32(&counter_32, val_32, val_32_new);
+    TEST_CHK(counter_32.value.val_32 == 200);
+    atomic_val_incr_32(&counter_32);
+    TEST_CHK(counter_32.value.val_32 == 201);
+    atomic_val_decr_32(&counter_32);
+    TEST_CHK(counter_32.value.val_32 == 200);
+    atomic_val_add_32(&counter_32, delta_32);
+    TEST_CHK(counter_32.value.val_32 = 210);
+    atomic_val_sub_32(&counter_32, delta_32);
+    TEST_CHK(counter_32.value.val_32 == 200);
+
+    atomic_val_t counter_16;
+    uint16_t val_16 = 0, val_16_new = 200;
+    int16_t delta_16 = 10;
+    atomic_val_init_16(&counter_16, val_16);
+    val_16 = 100;
+    atomic_val_store_16(&counter_16, val_16);
+    TEST_CHK(counter_16.value.val_16 == 100);
+    atomic_val_compare_and_set_16(&counter_16, val_16, val_16_new);
+    TEST_CHK(counter_16.value.val_16 == 200);
+    atomic_val_incr_16(&counter_16);
+    TEST_CHK(counter_16.value.val_16 == 201);
+    atomic_val_decr_16(&counter_16);
+    TEST_CHK(counter_16.value.val_16 == 200);
+    atomic_val_add_16(&counter_16, delta_16);
+    TEST_CHK(counter_16.value.val_16 = 210);
+    atomic_val_sub_16(&counter_16, delta_16);
+    TEST_CHK(counter_16.value.val_16 == 200);
+
+    atomic_val_t counter_8;
+    uint8_t val_8 = 0, val_8_new = 100;
+    int8_t delta_8 = 10;
+    atomic_val_init_8(&counter_8, val_8);
+    val_8 = 50;
+    atomic_val_store_8(&counter_8, val_8);
+    TEST_CHK(counter_8.value.val_8 == 50);
+    atomic_val_compare_and_set_8(&counter_8, val_8, val_8_new);
+    TEST_CHK(counter_8.value.val_8 == 100);
+    atomic_val_incr_8(&counter_8);
+    TEST_CHK(counter_8.value.val_8 == 101);
+    atomic_val_decr_8(&counter_8);
+    TEST_CHK(counter_8.value.val_8 == 100);
+    atomic_val_add_8(&counter_8, delta_8);
+    TEST_CHK(counter_8.value.val_8 = 110);
+    atomic_val_sub_8(&counter_8, delta_8);
+    TEST_CHK(counter_8.value.val_8 == 100);
+
+    TEST_RESULT("basic test");
+}
+
+struct worker_args {
+    atomic_val_t *counter_64;
+    atomic_val_t *counter_32;
+    atomic_val_t *counter_16;
+    atomic_val_t *counter_8;
+};
+
+void * worker(void *voidargs)
+{
+    struct worker_args *args = (struct worker_args*)voidargs;
+    int i = 0;
+    int64_t delta_64 = 10;
+    int32_t delta_32 = 5;
+    int16_t delta_16 = 3;
+    int8_t delta_8 = 1;
+
+    for (; i < 10000; ++i) {
+        atomic_val_incr_64(args->counter_64);
+        atomic_val_decr_64(args->counter_64);
+        atomic_val_add_64(args->counter_64, delta_64);
+        atomic_val_sub_64(args->counter_64, delta_64);
+
+        atomic_val_incr_32(args->counter_32);
+        atomic_val_decr_32(args->counter_32);
+        atomic_val_add_32(args->counter_32, delta_32);
+        atomic_val_sub_32(args->counter_32, delta_32);
+
+        atomic_val_incr_16(args->counter_16);
+        atomic_val_decr_16(args->counter_16);
+        atomic_val_add_16(args->counter_16, delta_16);
+        atomic_val_sub_16(args->counter_16, delta_16);
+
+        atomic_val_incr_8(args->counter_8);
+        atomic_val_decr_8(args->counter_8);
+        atomic_val_add_8(args->counter_8, delta_8);
+        atomic_val_sub_8(args->counter_8, delta_8);
+    }
+
+    thread_exit(0);
+    return NULL;
+}
+
+void multi_thread_test(int num_threads)
+{
+    TEST_INIT();
+
+    thread_t *tid = alca(thread_t, num_threads);
+    struct worker_args *args = alca(struct worker_args, num_threads);
+    void **ret = alca(void *, num_threads);
+
+    int i = 0;
+    atomic_val_t counter_64;
+    uint64_t val_64 = 0;
+    atomic_val_t counter_32;
+    uint32_t val_32 = 0;
+    atomic_val_t counter_16;
+    uint16_t val_16 = 0;
+    atomic_val_t counter_8;
+    uint8_t val_8 = 0;
+
+    atomic_val_init_64(&counter_64, val_64);
+    atomic_val_init_32(&counter_32, val_32);
+    atomic_val_init_16(&counter_16, val_16);
+    atomic_val_init_8(&counter_8, val_8);
+
+    for (; i < num_threads; ++i){
+        args[i].counter_64 = &counter_64;
+        args[i].counter_32 = &counter_32;
+        args[i].counter_16 = &counter_16;
+        args[i].counter_8 = &counter_8;
+        thread_create(&tid[i], worker, &args[i]);
+    }
+
+    for (i = 0; i < num_threads; ++i){
+        thread_join(tid[i], &ret[i]);
+    }
+
+    TEST_CHK(counter_64.value.val_64 == 0);
+    TEST_CHK(counter_32.value.val_32 == 0);
+    TEST_CHK(counter_16.value.val_16 == 0);
+    TEST_CHK(counter_8.value.val_8 == 0);
+
+    TEST_RESULT("multi thread test");
+}
+
+int main()
+{
+    basic_test();
+    multi_thread_test(8);
+
+    return 0;
+}
