1f60a5c9ca93cb21bce7e510743b0f770d965d32
Sundar Sridharan <sundar.sridharan@gmail.com>
2015-04-08 16:35:57 -0700

    MB-13701: Add tool forestdb_hexamine to debug fdb files
    This tool will read all blocks into memory and endian_decode
    DB header blocks for easy debugging of forestdb files in gdb.
    Change-Id: Ic84bc559bfc8171450ac5b228fd264dea7a4682b

---
+++ tools/dump_common.cc
@@ -0,0 +1,205 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2010 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include "dump_common.h"
+
+int _kvs_cmp_name_fdb_dump(struct avl_node *a,
+                                  struct avl_node *b,
+                                  void *aux)
+{
+    struct kvs_node *aa, *bb;
+    aa = _get_entry(a, struct kvs_node, avl_name);
+    bb = _get_entry(b, struct kvs_node, avl_name);
+    return strcmp(aa->kvs_name, bb->kvs_name);
+}
+
+void print_header(fdb_kvs_handle *db)
+{
+    uint8_t header_buf[FDB_BLOCKSIZE];
+    uint64_t ndocs;
+    uint64_t ndocs_wal_inserted, ndocs_wal_deleted;
+    uint64_t nlivenodes;
+    uint64_t datasize;
+    uint64_t datasize_wal;
+    uint64_t last_header_bid;
+    uint64_t kv_info_offset;
+    uint64_t header_flags;
+    size_t header_len;
+    size_t subblock_no, idx;
+    char *compacted_filename = NULL;
+    char *prev_filename = NULL;
+    bid_t bid;
+    bid_t trie_root_bid;
+    bid_t seq_root_bid;
+    fdb_seqnum_t seqnum;
+    filemgr_header_revnum_t revnum;
+
+    printf("DB header info:\n");
+
+    filemgr_get_header(db->file, header_buf, &header_len);
+    if (header_len > 0) {
+        fdb_fetch_header(header_buf, &trie_root_bid,
+                         &seq_root_bid, &ndocs, &nlivenodes,
+                         &datasize, &last_header_bid, &kv_info_offset,
+                         &header_flags, &compacted_filename, &prev_filename);
+        revnum = filemgr_get_header_revnum(db->file);
+
+        bid = filemgr_get_header_bid(db->file);
+        printf("    BID: %" _F64 " (0x%" _X64 ", byte offset: %" _F64 ")\n",
+               bid, bid, bid * FDB_BLOCKSIZE);
+        printf("    DB header length: %d bytes\n", (int)header_len);
+        printf("    DB header revision number: %d\n", (int)revnum);
+
+        if (trie_root_bid != BLK_NOT_FOUND) {
+            if (!is_subblock(trie_root_bid)) {
+                // normal block
+                printf("    HB+trie root BID: %" _F64 " (0x%" _X64 ", byte offset: %" _F64 ")\n",
+                       trie_root_bid, trie_root_bid, trie_root_bid * FDB_BLOCKSIZE);
+            } else {
+                // sub-block
+                subbid2bid(trie_root_bid, &subblock_no, &idx, &bid);
+                printf("    HB+trie root BID: %" _F64 ", %d-byte subblock #%zu",
+                       bid, db->bhandle->sb[subblock_no].sb_size, idx);
+                printf(" (0x%" _X64 ", byte offset: %" _F64 ")\n", trie_root_bid,
+                       bid * FDB_BLOCKSIZE + db->bhandle->sb[subblock_no].sb_size * idx);
+            }
+        } else {
+            printf("    HB+trie root BID: not exist\n");
+        }
+
+        if (seq_root_bid != BLK_NOT_FOUND) {
+            if (!is_subblock(seq_root_bid)) {
+                // normal block
+                printf("    Seq B+tree root BID: %" _F64 " (0x%" _X64 ", byte offset: %" _F64 ")\n",
+                       seq_root_bid, seq_root_bid, seq_root_bid * FDB_BLOCKSIZE);
+            } else {
+                // sub-block
+                subbid2bid(seq_root_bid, &subblock_no, &idx, &bid);
+                printf("    Seq B+tree root BID: %" _F64 ", %d-byte subblock #%zu",
+                       bid, db->bhandle->sb[subblock_no].sb_size, idx);
+                printf(" (0x%" _X64 ", byte offset: %" _F64 ")\n", seq_root_bid,
+                       bid * FDB_BLOCKSIZE + db->bhandle->sb[subblock_no].sb_size * idx);
+            }
+        } else {
+            printf("    Seq B+tree root BID: not exist\n");
+        }
+
+        if (last_header_bid != BLK_NOT_FOUND) {
+            printf("    DB header BID of the last WAL flush: %" _F64
+                   " (0x%" _X64 ", byte offset: %" _F64 ")\n",
+                   last_header_bid, last_header_bid, last_header_bid * FDB_BLOCKSIZE);
+        } else {
+            printf("    DB header BID of the last WAL flush: not exist\n");
+        }
+
+        if (db->config.multi_kv_instances) {
+            // multi KV instance mode
+            int i;
+            fdb_kvs_name_list name_list;
+            struct kvs_node *node, query;
+            struct avl_node *a;
+
+            ndocs = _kvs_stat_get_sum(db->file, KVS_STAT_NDOCS);
+            nlivenodes = _kvs_stat_get_sum(db->file, KVS_STAT_NLIVENODES);
+            ndocs_wal_inserted = wal_get_size(db->file);
+            ndocs_wal_deleted = wal_get_num_deletes(db->file);
+            datasize = _kvs_stat_get_sum(db->file, KVS_STAT_DATASIZE);
+            datasize_wal = wal_get_datasize(db->file);
+
+            printf("    # documents in the main index: %" _F64 " / "
+                   "in WAL: %" _F64 " (insert), %" _F64 " (remove)\n",
+                   ndocs, ndocs_wal_inserted, ndocs_wal_deleted);
+            printf("    # live index nodes: %" _F64 " (%" _F64 " bytes)\n",
+                   nlivenodes, nlivenodes * FDB_BLOCKSIZE);
+            printf("    Total document size: %" _F64 " bytes, (index: %" _F64 " bytes, "
+                   "WAL: %" _F64 " bytes)\n",
+                   datasize + datasize_wal, datasize, datasize_wal);
+
+            fdb_get_kvs_name_list(db->fhandle, &name_list);
+
+            printf("    # KV stores: %d\n", (int)name_list.num_kvs_names);
+            for (i=0; i<name_list.num_kvs_names; ++i){
+                if (strcmp(name_list.kvs_names[i], DEFAULT_KVS_NAME)) {
+                    query.kvs_name = name_list.kvs_names[i];
+                    a = avl_search(db->file->kv_header->idx_name,
+                                   &query.avl_name,
+                                   _kvs_cmp_name_fdb_dump);
+                    if (!a) {
+                        continue;
+                    }
+
+                    printf("      KV store name: %s\n", name_list.kvs_names[i]);
+                    node = _get_entry(a, struct kvs_node, avl_name);
+                    seqnum = node->seqnum;
+                    ndocs = node->stat.ndocs;
+                    nlivenodes = node->stat.nlivenodes;
+                    ndocs_wal_inserted = node->stat.wal_ndocs - node->stat.wal_ndeletes;
+                    ndocs_wal_deleted = node->stat.wal_ndeletes;
+                    datasize = node->stat.datasize;
+                } else { // default KVS
+                    printf("      KV store name: %s\n", name_list.kvs_names[i]);
+                    ndocs = db->file->header.stat.ndocs;
+                    nlivenodes = db->file->header.stat.nlivenodes;
+                    seqnum = db->file->header.seqnum;
+                    ndocs_wal_inserted = db->file->header.stat.wal_ndocs -
+                                         db->file->header.stat.wal_ndeletes;
+                    ndocs_wal_deleted = db->file->header.stat.wal_ndeletes;
+                    datasize = db->file->header.stat.datasize;
+                }
+
+                printf("      # documents in the main index: %" _F64 " / "
+                       "in WAL: %" _F64 " (insert), %" _F64 " (remove)\n",
+                       ndocs, ndocs_wal_inserted, ndocs_wal_deleted);
+                printf("      # live index nodes: %" _F64 " (%" _F64 " bytes)\n",
+                       nlivenodes, nlivenodes * FDB_BLOCKSIZE);
+                printf("      Total document size: %" _F64 " bytes\n", datasize);
+                printf("      Last sequence number: %" _F64 "\n", seqnum);
+                printf("\n");
+            }
+
+            fdb_free_kvs_name_list(&name_list);
+
+        } else {
+            // single KV instance mode
+            seqnum = filemgr_get_seqnum(db->file);
+            ndocs_wal_inserted = wal_get_size(db->file);
+            ndocs_wal_deleted = wal_get_num_deletes(db->file);
+            datasize_wal = wal_get_datasize(db->file);
+
+            printf("    # documents in the main index: %" _F64 " / "
+                   "in WAL: %" _F64 " (insert), %" _F64 " (remove)\n",
+                   ndocs, ndocs_wal_inserted, ndocs_wal_deleted);
+            printf("    # live index nodes: %" _F64 " (%" _F64 " bytes)\n",
+                   nlivenodes, nlivenodes * FDB_BLOCKSIZE);
+            printf("    Total document size: %" _F64 " bytes, (index: %" _F64 " bytes, "
+                   "WAL: %" _F64 " bytes)\n",
+                   datasize + datasize_wal, datasize, datasize_wal);
+            printf("    Last sequence number: %" _F64 "\n", seqnum);
+        }
+
+        if (compacted_filename) {
+            printf("    Next file after compaction: %s\n", compacted_filename);
+        }
+        if (prev_filename) {
+            printf("    Previous file before compaction: %s\n", prev_filename);
+            free(prev_filename);
+        }
+
+    } else {
+        printf("    No header exists.\n");
+    }
+}
---
+++ tools/forestdb_hexamine.cc
@@ -0,0 +1,333 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2010 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include "dump_common.h"
+
+#define MAX_FILE_SIZE 1610612736 // 1.5 GB
+/**
+ * How to Use this tool to examine forestdb files with help of gdb
+ * ---------------------------------------------------------------
+ * This tool will read ALL the blocks of the file into the db[] array
+ * and loop forever. gdb can be attached to the process to examine blocks.
+ * Example..
+ *   bash$ gdb ./forestdb_hexamine
+ *     (gdb) r path.to.forestdb.file
+ * It will print all DB header indexes as it reads
+ * DB Header 1
+ * DB Header 3
+ * DB Header 106
+ * File scanned and headers decoded
+ * gdb --pid=6575
+ * Process 6575 stopped
+ * p db[106]
+ * (dblock) $0 = {
+ *   trie_root_raw = 9007199254741048
+ *   seqtree_root_raw = 9288674231451704
+ *   num_docs = 1
+ *   num_nodes = 0
+ *   data_size = 24371
+ *   last_wal_flush_bid = 106
+ *   kv_info_offset = 229176
+ *    = (hdr_flags = 3, flags = void * = 0x0000000000000003)
+ *   new_filename_len = 0
+ *   old_filename_len = 0
+ *   bytes = "y\x85I\x9b"
+ *   trie_root_bid = 56
+ *   trie_subblock_no = 0
+ *   trie_idx = 0
+ *   seqtree_root_bid = 56
+ *   seqtree_subblock_no = 0
+ *   seqtree_idx = 1
+ *   prev_hdr_bid = 3
+ *   hdr_len = 72
+ *  = (magic_bytes = 16045704242864832239, magic = void * = 0xdeadcafebeefbeef)
+ *   marker = '?'
+ * }
+ *
+ */
+void print_usage(void)
+{
+    printf("\nUsage: forestdb_hexamine [OPTION]... [filename]\n"
+    "\n-  Loads entire file in memory and hangs in gdb_loop for examination\n"
+    "\nOptions:\n"
+    "\n      --print-header     displays KV info (for non-corrupted files)"
+    "\n      --headers-only     only dump contents of all DB headers and exit"
+    "\n      --max-filesize     reset maximum file size (default 1.5GB)"
+    "\n");
+}
+
+struct input_option{
+    char *filename;
+    bool print_header;
+    bool headers_only;
+    uint64_t max_filesize;
+};
+
+enum blk_type {
+    BNODE_BLK = 0xff,
+    DBHEADER_BLK = 0xee,
+    DOC_BLK = 0xdd
+};
+
+#define BLK_SIZE 4096
+#pragma pack(1)
+
+typedef
+struct db_header {
+    uint64_t trie_root_raw;
+    uint64_t seqtree_root_raw;
+    uint64_t num_docs;
+    uint64_t num_nodes;
+    uint64_t data_size;
+    uint64_t last_wal_flush_bid;
+    uint64_t kv_info_offset;
+    union {
+        uint64_t hdr_flags;
+        void    *flags;
+    };
+    uint16_t new_filename_len;
+    uint16_t old_filename_len;
+
+    char bytes[3961];
+
+
+    uint64_t trie_root_bid;
+    uint64_t trie_subblock_no;
+    uint64_t trie_idx;
+    uint64_t seqtree_root_bid;
+    uint64_t seqtree_subblock_no;
+    uint64_t seqtree_idx;
+    uint64_t prev_hdr_bid;
+    uint16_t hdr_len;
+    union {
+        uint64_t magic_bytes;
+        void   *magic;
+    };
+    uint8_t marker;
+}dblock;
+
+void decode_dblock(void *block) {
+    dblock *_db = (dblock *)block;
+    size_t subblock_no, idx;
+    _db->trie_root_raw = _endian_decode(_db->trie_root_raw);
+    _db->seqtree_root_raw =_endian_decode(_db->seqtree_root_raw);
+    _db->num_docs = _endian_decode(_db->num_docs);
+    _db->num_nodes = _endian_decode(_db->num_nodes);
+    _db->data_size = _endian_decode(_db->data_size);
+    _db->last_wal_flush_bid = _endian_decode(_db->
+            last_wal_flush_bid);
+    _db->kv_info_offset = _endian_decode(_db->kv_info_offset);
+    _db->hdr_flags = _endian_decode(_db->hdr_flags);
+
+    _db->new_filename_len = _endian_decode(_db->new_filename_len);
+    _db->old_filename_len = _endian_decode(_db->old_filename_len);
+    if (!is_subblock(_db->trie_root_raw)) {
+        _db->trie_root_bid = _db->trie_root_raw;
+        _db->trie_subblock_no = 0;
+        _db->trie_idx = 0;
+    } else {
+        subbid2bid(_db->trie_root_raw,
+                   &subblock_no,
+                   &idx,
+                   &_db->trie_root_bid);
+        _db->trie_subblock_no = subblock_no;
+        _db->trie_idx = idx;
+    }
+    if (!is_subblock(_db->seqtree_root_raw)) {
+        _db->seqtree_root_bid = _db->trie_root_raw;
+        _db->seqtree_subblock_no = 0;
+        _db->seqtree_idx = 0;
+    } else {
+        subbid2bid(_db->seqtree_root_raw,
+                   &subblock_no,
+                   &idx,
+                   &_db->seqtree_root_bid);
+        _db->seqtree_subblock_no = subblock_no;
+        _db->seqtree_idx = idx;
+    }
+    _db->prev_hdr_bid = _endian_decode(_db->prev_hdr_bid);
+    _db->magic_bytes = _endian_decode(_db->magic_bytes);
+    _db->hdr_len = _endian_decode(_db->hdr_len);
+}
+
+dblock *db;
+
+void gdb_sleep(void) {
+    usleep(1000);
+}
+
+int process_file(struct input_option *opt)
+{
+    fdb_file_handle *dbfile;
+    fdb_config config;
+    char *filename = opt->filename;
+    uint64_t file_size;
+    size_t num_blocks;
+    struct filemgr *file;
+    uint8_t block_buf[BLK_SIZE];
+    fdb_status fs;
+
+    config = fdb_get_default_config();
+    config.buffercache_size = 0;
+    config.flags = FDB_OPEN_FLAG_RDONLY;
+    fs = fdb_open(&dbfile, filename, &config);
+    if (fs != FDB_RESULT_SUCCESS) {
+        printf("\nUnable to open %s\n", filename);
+        return -1;
+    }
+
+    file = dbfile->root->file;
+    file_size = file->pos.val;
+    num_blocks = file_size / BLK_SIZE;
+
+    if (opt->print_header) {
+        print_header(dbfile->root);
+    }
+    if (file_size > opt->max_filesize) {
+        printf("\n File %s size %" _F64" exceeds max size of %"_F64" \n",
+               filename, file_size, opt->max_filesize);
+        fdb_close(dbfile);
+        return -1;
+    }
+
+    if (opt->headers_only) {
+        for (uint64_t i = 0; i < num_blocks; ++i) {
+            ssize_t rv = file->ops->pread(file->fd, &block_buf, BLK_SIZE,
+                                          i * BLK_SIZE);
+            if (rv != BLK_SIZE) {
+                fdb_close(dbfile);
+                return FDB_RESULT_READ_FAIL;
+            }
+            db = (dblock *)&block_buf;
+
+            if (db->marker == DBHEADER_BLK) {
+                printf("\n-------DB Header %" _F64 " at offset %" _F64
+                        "--------" , i, i * BLK_SIZE);
+                decode_dblock(db);
+                printf("\ntrie_root_raw = %p", (void*)db->trie_root_raw);
+                printf("\nseqtree_root_raw = %p", (void*)db->seqtree_root_raw);
+                printf("\nnum_docs = %" _F64, db->num_docs);
+                printf("\nnum_nodes = %" _F64, db->num_nodes);
+                printf("\ndatasize = %" _F64, db->data_size);
+                printf("\nlast_wal_flush_bid = %" _F64, db->last_wal_flush_bid);
+                printf("\nkv_info_offset = %" _F64, db->kv_info_offset);
+                printf("\nflags = %p", db->flags);
+                printf("\nnew_filename_len = %d", db->new_filename_len);
+                printf("\nold_filename_len = %d", db->old_filename_len);
+                printf("\ntrie_root_bid = %" _F64, db->trie_root_bid);
+                printf("\ntrie_subblock_no = %" _F64, db->trie_subblock_no);
+                printf("\ntrie_idx = %" _F64, db->trie_idx);
+                printf("\nseqtree_root_bid = %" _F64, db->seqtree_root_bid);
+                printf("\nseqtree_subblock_no = %" _F64,
+                                                 db->seqtree_subblock_no);
+                printf("\nseqtree_idx = %" _F64, db->seqtree_idx);
+                printf("\nprev_hdr_bid = %" _F64, db->prev_hdr_bid);
+                printf("\nhdr_len = %d", db->hdr_len);
+                printf("\nmagic = %p", db->magic);
+            }
+        }
+
+    } else {
+        db = (dblock *) malloc(file_size);
+        if (!db) {
+            printf("\nUnable to allocate memory of %" _F64" bytes\n",
+            file_size);
+            fdb_close(dbfile);
+            return -1;
+        }
+        for (uint64_t i = 0; i < num_blocks; ++i) {
+            ssize_t rv = file->ops->pread(file->fd, &db[i], BLK_SIZE,
+                    i * BLK_SIZE);
+            if (rv != BLK_SIZE) {
+                fdb_close(dbfile);
+                free(db);
+                return FDB_RESULT_READ_FAIL;
+            }
+            if (db[i].marker == DBHEADER_BLK) {
+                printf("\nDB Header %" _F64, i);
+                decode_dblock(&db[i]);
+            }
+        }
+#if !defined(WIN32) && !defined(_WIN32)
+        printf("\nFile scanned and headers decoded\n gdb --pid=%d\n", getpid());
+#endif
+        while (1) {
+            gdb_sleep();
+        }
+        free(db);
+    }
+
+    fs = fdb_close(dbfile);
+    if (fs != FDB_RESULT_SUCCESS) {
+        printf("\nUnable to close %s\n", filename);
+        return -4;
+    }
+
+    return -1;
+}
+
+int parse_options(int argc, char **argv, struct input_option *opt)
+{
+    // Unfortunately, we cannot use getopt generally
+    // because Windows doesn't support it ..
+    int i = 1;
+
+    if (argc < 2) {
+        print_usage();
+        return -1;
+    }
+
+    for (i = 1; i < argc; ++i) {
+        if (argv[i][0] == '-' && argv[i][1] == '-') {
+            if (strncmp(argv[i], "--print-header", 16) == 0) {
+                opt->print_header = true;
+            } else if (strncmp(argv[i], "--headers-only", 16) == 0) {
+                opt->headers_only = true;
+            } else if (strncmp(argv[i], "--max-filesize", 16) == 0) {
+                sscanf(argv[++i], "%"_F64, &opt->max_filesize);
+            } else {
+                printf("\nUnknown option %s\n", argv[i]);
+                print_usage();
+                return -2;
+            }
+        } else {
+            opt->filename = argv[i];
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    struct input_option opt;
+    memset(&opt, 0 , sizeof(struct input_option));
+    opt.max_filesize = MAX_FILE_SIZE;
+    memleak_start();
+    int ret = parse_options(argc, argv, &opt);
+
+    if (ret) {
+        memleak_end();
+        return ret;
+    }
+
+    ret = process_file(&opt);
+
+    fdb_shutdown();
+    memleak_end();
+    return ret;
+}
