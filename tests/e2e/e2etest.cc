85e321feb51e29d97c477c63826e2a11cf9b0640
Sundar Sridharan <sundar.sridharan@gmail.com>
2015-09-28 11:52:57 -0700

    MB-16249: fix e2etest iterator_get & free ptr correctly
    Change-Id: Ie57efda919d572f94e1fefb4e876cd1b4acf8f33

--- tests/e2e/e2etest.cc
+++ tests/e2e/e2etest.cc
@@ -454,9 +454,9 @@ void *iterate_thread(void *args){
             status = fdb_iterator_get(it, &rdoc);
             if (j) {
                 TEST_CHK(status == FDB_RESULT_SUCCESS);
-                fdb_doc_free(rdoc);
-                rdoc = NULL;
             }
+            fdb_doc_free(rdoc);
+            rdoc = NULL;
             j++;
         } while (fdb_iterator_next(it) != FDB_RESULT_ITERATOR_FAIL);
         // reverse and seek ahead
