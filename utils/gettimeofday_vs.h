f9bcfdd2f36fcadd002d52afc9a5bcc71e6e76eb
topilski <atopilski@gmail.com>
2017-07-25 08:29:04 +0300

    Mingw32 compiler support

--- utils/gettimeofday_vs.h
+++ utils/gettimeofday_vs.h
@@ -4,13 +4,15 @@
 #include <winsock2.h>
 #undef NOMINMAX
 #include <time.h>
-#include <Windows.h>
+#include <windows.h>
 
+#ifndef __MINGW32__
 struct timezone
 {
     int  tz_minuteswest; /* minutes W of Greenwich */
     int  tz_dsttime;     /* type of dst correction */
 };
+#endif
 
 int gettimeofday_vs(struct timeval *tv, void *tz);
 
