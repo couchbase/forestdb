49cf5bcb56030afe178a2f798a0fbe3702c66a30
Dave Rigby <daver@couchbase.com>
2021-06-28 09:17:15 +0100

    MB-47145: Only install signal handler on x86
    The signal handler code in forestDB is x86-specific. Don't install it
    on non-x86 architectures; relying on whatever higher-level signal
    handlers the application has (Breakpad etc).
    
    Change-Id: I0b9c5f15f898ab636802b833f9f5e1f1012cf4da
    Reviewed-on: http://review.couchbase.org/c/forestdb/+/156528
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- utils/debug.cc
+++ utils/debug.cc
@@ -69,7 +69,8 @@ static void _dbg_hang_process(void)
 
 // to profile first install perf
 // echo 0 > /proc/sys/kernel/kptr_restrict
-#if defined(__linux__) && !defined(__ANDROID__)
+#if defined(__linux__) && !defined(__ANDROID__) && \
+        (defined(__i386__) || defined(__x86_64__))
 #include <string.h>
 #include <dlfcn.h>
 #include <ucontext.h>
